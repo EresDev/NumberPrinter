89746a9e485ee0993ce42d3c8c069fd854aa6f20
EresDev <arslanafzal321@gmail.com>
2019-02-18 20:12:56 +0100

    Number printer program

---
+++ NumberPrinter.php
@@ -0,0 +1,13 @@
+<?php
+
+class NumberPrinter
+{
+    public static function execute()
+    {
+        for ($i = 0; $i < 5; ++$i) {
+            echo $i . "\n";
+        }
+    }
+}
+
+NumberPrinter::execute();
